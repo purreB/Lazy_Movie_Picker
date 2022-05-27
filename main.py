73207190093ac904e48d0a72ebc0bc3943227afd
purreB <jakob.bergstrom@live.se>
2022-05-27 22:06:19 +0200

    made my first external api request, returns response as json

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from settings import SECRET_KEY
+import asyncio
+import httpx
 
 app = FastAPI()
+
+url = "https://imdb-api.com/API/Usage/k_ujvg6j6a"
 
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to my lazy_movie_picker backend!"}
+
+# Sends a asyncronous request to imdb api and returns the response as json
+async def test():
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://imdb-api.com/API/Usage/k_ujvg6j6a")
+        r_data = r.json()
+        return r_data
+
+# Executes test() function and returns the response to our frontend
+@app.get("/imdbTest")
+async def idk():
+    cool = await test()
+    return cool
