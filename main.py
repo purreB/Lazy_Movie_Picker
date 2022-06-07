2eba1b958598188f9666ae56e99d242a2e3fa07a
purreB <jakob.bergstrom@live.se>
2022-06-07 15:41:55 +0200

    now calls api using secret key...that actually is secret

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
-from settings import SECRET_KEY
 import asyncio
+
 import httpx
+from fastapi import FastAPI
+
+from settings import SECRET_KEY
 
 app = FastAPI()
 
-url = "https://imdb-api.com/API/Usage/k_ujvg6j6a"
+url = "https://imdb-api.com/API/Usage/{secret}".format(secret=SECRET_KEY)
 
 
 @app.get("/")
@@ -13,13 +15,17 @@ async def root():
     return {"message": "Welcome to my lazy_movie_picker backend!"}
 
 # Sends a asyncronous request to imdb api and returns the response as json
+
+
 async def test():
     async with httpx.AsyncClient() as client:
-        r = await client.get("https://imdb-api.com/API/Usage/k_ujvg6j6a")
+        r = await client.get(url)
         r_data = r.json()
         return r_data
 
 # Executes test() function and returns the response to our frontend
+
+
 @app.get("/imdbTest")
 async def idk():
     cool = await test()
