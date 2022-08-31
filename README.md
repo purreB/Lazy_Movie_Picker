ea6e464981959fb2de03e35b40392b74ad608d12
purreB <jakob.bergstrom@live.se>
2022-08-31 22:39:22 +0200

    added first dynamic functionality, can now dynamically search moveis by title and it fetches it from imdb API

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lazy_Movie_Picker
+
 A (eventually) webapp, built with python (FastAPI) as backend and not yet sure about the Frontend, that fetches movies from IMDB and picks out movies or series depending on user interaction.
+
+# How to spin it up:
+
+- use hypercorn main:app
--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from settings import SECRET_KEY
 
 app = FastAPI()
 
-url = "https://imdb-api.com/API/Usage/{secret}".format(secret=SECRET_KEY)
+TEST_URL = "https://imdb-api.com/API/Usage/{secret}".format(secret=SECRET_KEY)
+
+BASE_API_URL = "https://imdb-api.com/API/"
 
 
 @app.get("/")
@@ -19,7 +21,7 @@ async def root():
 
 async def test():
     async with httpx.AsyncClient() as client:
-        r = await client.get(url)
+        r = await client.get(TEST_URL)
         r_data = r.json()
         return r_data
 
@@ -30,3 +32,17 @@ async def test():
 async def idk():
     cool = await test()
     return cool
+
+
+async def dynamicSearch(url: str):
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url)
+        r_data = r.json()
+        return r_data
+
+
+@app.get("/searchByTitle/{title}")
+async def SEARCH_BY_TITLE(title: str):
+    search_string = BASE_API_URL + "SearchTitle/" + SECRET_KEY + "/" + title
+    response = await dynamicSearch(url=search_string)
+    return response
